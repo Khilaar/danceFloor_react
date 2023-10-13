40c200f08aa82080d3c821135c99a68a1253d93a
Khilaar <luca.oliverio@hotmail.com>
2023-10-13 21:58:08 +0200

    Upload of the full solution of my simple dancefloor, the bonus will be a own project, I dont want to destroy this ^^

--- src/components/gridFolder/grid.jsx
+++ src/components/gridFolder/grid.jsx
@@ -22,23 +22,24 @@ function Grid() {
     const [currentInterval, changeInterval] = useState(0)
 
     const handleClick = () => {
-        clearInterval(currentInterval)
-        let interval = setInterval(() => setRgbColors(generateColors()), 500)
-        changeInterval(interval) 
+        if (currentInterval) {
+            clearInterval(currentInterval);
+            changeInterval(null);
+        } else {
+            clearInterval(currentInterval);
+            let interval = setInterval(() => setRgbColors(generateColors()), 500);
+            changeInterval(interval);
+        }
     }
 
-    const handleStop = () => {
-        clearInterval(currentInterval)
-        changeInterval(0);
-    }
+    
 
     return (
         <div className='gridContainer'>
             <div className='grid'>
                 {rgbColors.map(color => <Cells color={color} key={color}>Cell</Cells>)}
             </div>
-            <button onClick={handleClick}>Im a Button</button>
-            <button onclick={handleStop}>Stop the wild dancing</button>
+            <button onClick={handleClick}>Start/Stop Party</button>
         </div>
     )
 }
